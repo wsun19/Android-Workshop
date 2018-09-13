3639addb7dbd5278e812f20718f2ef99ef5972f2
William Sun <mod.william@gmail.com>
2018-09-12 21:09:17 -0700

    Basic RecyclerView

--- MyDemo/app/src/main/java/com/google/williamsun/mydemo/MainActivity.java
+++ MyDemo/app/src/main/java/com/google/williamsun/mydemo/MainActivity.java
@@ -3,15 +3,33 @@ package com.google.williamsun.mydemo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.google.williamsun.mydemo.MESSAGE";
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
     @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
+            mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            mRecyclerView.setHasFixedSize(true);
+
+            // use a linear layout manager
+            mLayoutManager = new LinearLayoutManager(this);
+            mRecyclerView.setLayoutManager(mLayoutManager);
+
+            // specify an adapter (see also next example)
+            mAdapter = new MyAdapter(myDataset);
+            mRecyclerView.setAdapter(mAdapter);
         }
 
         /** Called when the user taps the Send button */
@@ -20,6 +38,6 @@ public class MainActivity extends AppCompatActivity {
             EditText editText = (EditText) findViewById(R.id.editText);
             String message = editText.getText().toString();
             intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+            startActivity(intent);
     }
 }
